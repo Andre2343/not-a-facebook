2b0debca6a2c97369bdd3dd13339ccd8a14b165e
ВашÐKos Andriy <andriy_kos_dfc@ukr.net>
2019-10-30 16:52:38 +0200

    replacement of the class UserPage by a hooks

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,28 +1,27 @@
-import React from 'react';
+/* eslint-disable no-undef */
+// eslint-disable-next-line react/prefer-stateless-function
+import React, { useState } from 'react';
 import {
   Card, CardImg, CardBody, CardTitle,
 } from 'reactstrap';
 import UserModel from '../modules/users';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class UserPage extends React.Component {
-  render() {
-    const { match: { params: { username } } } = this.props;
-    const user = UserModel.getUserByUsername(username);
-    // const user = UserModel.getUser(id);
-    const { profile } = user;
-    return (
-      <div>
-        <Card>
-          <CardImg style={{ width: 200, height: 200 }} className="avatar" src={profile.avatar} />
-          <CardBody>
-            <CardTitle>{profile.firstName}</CardTitle>
-            <CardTitle>{profile.lastName}</CardTitle>
-          </CardBody>
-        </Card>
-      </div>
-    );
-  }
+function UserPage() {
+  const { match: { params: { username } } } = useState(props);
+  const user = UserModel.getUserByUsername(username);
+  // const user = UserModel.getUser(id);
+  const { profile } = user;
+  return (
+    <div>
+      <Card>
+        <CardImg style={{ width: 200, height: 200 }} className="avatar" src={profile.avatar} />
+        <CardBody>
+          <CardTitle>{profile.firstName}</CardTitle>
+          <CardTitle>{profile.lastName}</CardTitle>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
 
 export default UserPage;
